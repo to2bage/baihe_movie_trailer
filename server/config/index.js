f01bcd40fe08111aa17cd85164372e37cbc0ecec
to2bage <to2bage@hotmail.com>
2018-12-15 15:03:35 +0800

    删除ak,sk

--- server/config/index.js
+++ server/config/index.js
@@ -2,7 +2,7 @@ module.exports = {
     'qiniu': {
       'bucket': 'movies',
       'video': 'http://pjb2hpvdr.bkt.clouddn.com/',
-      'AK': 'f7_m97GFShwT2FUxrTR4V0gEd7KHQ1kz9PAqyYsu',
-      'SK': '9wqrxMQs8kuFxWbpx9WVuT_JJH0wwUavSOSFViqg'
+      'AK': 'XXXXXXX',
+      'SK': 'YYYYYYYY'
     }
   }
